d4b86e3a17384078da70fd05fae0834e6239b349
Thor Smith <thorsmith777@gmail.com>
2024-09-08 22:53:16 -0400

    adds data, data parsing, and training
    This change adds a csv file with some sample data, populates the
    function for reading in the data, and updates the training function
    calls (compile/fit) so that the referee model generator actually runs
    and trains a model.

--- tic_tac_toe_referee_model_generator.py
+++ tic_tac_toe_referee_model_generator.py
@@ -2,32 +2,51 @@
 
 import os
 import sys
+import csv
 import numpy as np
 import tensorflow as tf
 
 class TicTacToeReferee(object):
     NUM_EPOCHS=5
     def __init__(self):
-        input_size = 9
-        hidden_size = 15
-        output_size = 5
+        self.input_size = 9
+        self.hidden_size = 15
+        self.output_size = 5
         self.model = tf.keras.Sequential([
-            tf.keras.layers.Input(shape=(input_size)),
-            tf.keras.layers.Dense(hidden_size, activation='relu'),
-            tf.keras.layers.Dense(output_size, activation='relu'),
+            tf.keras.layers.Input(shape=(self.input_size)),
+            tf.keras.layers.Dense(self.hidden_size, activation='relu'),
+            tf.keras.layers.Dense(self.output_size, activation='relu'),
             ])
-        self.train_data = []
-        self.test_data = []
-    def import_training_data(self):
-        pass
-    def train(self, epochs=self.NUM_EPOCHS):
-        self.model.compile(optimizer='SGD', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-        self.model.fit(self.train_data, epochs, validation_data=([], []), verbose=2)
+        self.data_dict = None
+        self.input_data = None
+        self.output_data = None
+    def import_training_data(self, filename="TicTacToeBoardClassifierData.csv"):
+        input_keys = ["r1c1","r1c2","r1c3","r2c1","r2c2","r2c3","r3c1","r3c2","r3c3"]
+        output_keys = ["xwon","owon","tie","invalid","incomplete"]
+        keys = input_keys + output_keys
+        with open(filename, newline='') as datafile:
+            reader = csv.DictReader(datafile)
+            self.data_dict = reader
+            input_list = []
+            output_list = []
+            for row in reader:
+                input_row = [ float(row[x]) for x in input_keys ]
+                input_list.append(input_row)
+                output_row = [ float(row[x]) for x in output_keys ] 
+                output_list.append(output_row)
+            self.input_data = tf.constant(input_list, dtype=float)
+            print(self.input_data)
+            self.output_data = tf.constant(output_list, dtype=float)
+            print(self.output_data)
+    def train(self, epochs=NUM_EPOCHS):
+        self.model.compile(optimizer='SGD', loss='categorical_crossentropy', metrics=['accuracy'])
+        self.model.fit(self.input_data, self.output_data, batch_size=1, epochs=epochs, validation_split=0.10, shuffle=True, verbose=2)
 
 
 def main() -> int:
     """Interact with the TicTacToeReferee AI model"""
     referee = TicTacToeReferee()
+    referee.import_training_data()
     referee.train()
     return 0
 
