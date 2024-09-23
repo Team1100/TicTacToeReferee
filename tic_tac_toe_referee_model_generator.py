db66c74f05fb20c2324870c7b6b2898751631d7d
Thor Smith <thorsmith777@gmail.com>
2024-09-22 21:17:53 -0400

    adds verbose and learning rate options
    This change cleans up the printing during training and model use by
    wrapping unnecessary prints in a verbosity flag. The change also exposes
    the learning rate as a command line option to facilitate a better
    experimentation experience.

--- tic_tac_toe_referee_model_generator.py
+++ tic_tac_toe_referee_model_generator.py
@@ -12,7 +12,7 @@ class TicTacToeReferee(object):
     INPUT_KEYS = ["r1c1","r1c2","r1c3","r2c1","r2c2","r2c3","r3c1","r3c2","r3c3"]
     OUTPUT_KEYS = ["xwon","owon","tie","invalid","incomplete"]
     NUM_EPOCHS=30
-    def __init__(self, output_keys=OUTPUT_KEYS, epochs=NUM_EPOCHS):
+    def __init__(self, output_keys=OUTPUT_KEYS, epochs=NUM_EPOCHS, verbose=False):
         self.input_size = 9
         self.hidden_size = 15
         self.output_size = len(output_keys)
@@ -27,6 +27,7 @@ class TicTacToeReferee(object):
         self.input_data = None
         self.output_data = None
         self.epochs = epochs
+        self.verbose = verbose
     def import_training_data(self, filename="TicTacToeBoardClassifierData.csv", output_keys=OUTPUT_KEYS):
         input_keys = self.INPUT_KEYS
         keys = input_keys + self.output_keys
@@ -41,14 +42,17 @@ class TicTacToeReferee(object):
                 output_row = [ float(row[x]) for x in self.output_keys ] 
                 output_list.append(output_row)
             self.input_data = tf.constant(input_list, dtype=float)
-            print(self.input_data)
+            self.verbose_print(self.input_data)
             self.output_data = tf.constant(output_list, dtype=float)
-            print(self.output_data)
-    def train(self):
-        self.model.compile(keras.optimizers.SGD(learning_rate=0.8), loss='mean_squared_error', metrics=['accuracy'])
-        self.model.fit(self.input_data, self.output_data, batch_size=10, epochs=self.epochs, validation_split=0.05, shuffle=True, verbose=2)
+            self.verbose_print(self.output_data)
+    def train(self, learning_rate=1.0):
+        self.model.compile(keras.optimizers.SGD(learning_rate=learning_rate), loss='mean_squared_error', metrics=['accuracy'])
+        self.model.fit(self.input_data, self.output_data, batch_size=10, epochs=self.epochs, validation_split=0.001, shuffle=True, verbose=2)
     def save(self, filename):
         self.model.save(filename)
+    def verbose_print(self, text):
+        if self.verbose:
+            print(text)
 
 def ticTacToeBoard(s):
     board = [float(v) for v in s.split(',')]
@@ -59,9 +63,11 @@ def ticTacToeBoard(s):
 def main() -> int:
     """Interact with the TicTacToeReferee AI model"""
     ap = argparse.ArgumentParser(description='A programe for interacting with a TicTacToeReferee AI model')
+    ap.add_argument("--verbose", action='store_true', help="Print extra debug information")
     ap.add_argument("--data-file", default="TicTacToeBoardClassifierData.csv", help="A CSV file containing rows with 9 columns of data input and 5 expected outputs")
     ap.add_argument("-o", "--output", choices=TicTacToeReferee.OUTPUT_KEYS, default=None, action='append', help="The set of outputs to to use when training the model.")
     ap.add_argument("--epochs", type=int, default=TicTacToeReferee.NUM_EPOCHS, help="The number of epochs to use for training.")
+    ap.add_argument("--learning-rate", type=float, default=1.0, help="The learning rate used for training.")
     ap.add_argument("--save-file", default="tic_tac_toe_referee.keras", help="The location to save the keras model after training")
     ap.add_argument("--load-file", default="tic_tac_toe_referee.keras", help="The location to load the keras model from to use it")
     ap.add_argument("--action", default='train', choices=['train', 'use'], help="The action to be performed on the model")
@@ -72,16 +78,18 @@ def main() -> int:
         output_keys = TicTacToeReferee.OUTPUT_KEYS
         if pa.output:
             output_keys = pa.output
-        referee = TicTacToeReferee(output_keys=output_keys, epochs=pa.epochs)
+        referee = TicTacToeReferee(output_keys=output_keys, epochs=pa.epochs, verbose=pa.verbose)
         referee.import_training_data(filename=pa.data_file)
-        referee.train()
+        referee.train(pa.learning_rate)
         referee.save(pa.save_file)
     else:
         model = keras.models.load_model(pa.load_file)
         in_tensor = tf.constant([pa.input_board], dtype=float)
-        print(in_tensor)
+        print("Input:      " + str(in_tensor))
         output = model(in_tensor)
-        print(output)
+        if pa.verbose:
+            print("Output Raw: " + str(output))
+        print("Output:     " + str(tf.round(output)))
     return 0
 
 if __name__ == '__main__':
